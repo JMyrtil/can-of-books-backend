695f3613846a4573d1b283e73849d440f3595276
MichaelDulin <michaeldulin1211@gmail.com>
2023-03-15 02:11:16 +0000

    added POST / DELETE

--- server.js
+++ server.js
@@ -13,12 +13,17 @@ db.once('open', function() {
 });
 mongoose.connect(process.env.DB_URL);
 
+
 const app = express();
 app.use(cors());
 
+app.use(express.json());
 const PORT = process.env.PORT || 3002;
 
 app.get('/books', getBooks);
+app.post('/books', postBooks);
+app.delete('/books/:id', deleteBooks);
+
 
 async function getBooks(req, res, next) {
   try {
@@ -28,6 +33,27 @@ async function getBooks(req, res, next) {
     next(arr);
   }
 }
+
+async function postBooks(req, res, next) {
+  console.log(req.body);
+  try {
+    let createdBook = await Book.create(req.body);
+    req.status(200).send(createdBook);
+  } catch(err) {
+    next(err);
+  }
+}
+
+async function deleteBooks(req, res, next) {
+  try {
+    let id = req.params.id;
+    await Book.findByIdAndDelete(id);
+    req.status(200).send('Book deleted');
+  } catch(err) {
+    next(err);
+  }
+}
+
 
 
 app.get('/', (request, response) => {
